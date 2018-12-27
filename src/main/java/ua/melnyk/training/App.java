a60dcf5425a347376dbb481405bc942515952a52
Hennadii Melnyk <erklareb@outlook.com>
2018-12-27 15:13:00 +0200

    4

--- src/main/java/ua/melnyk/training/App.java
+++ src/main/java/ua/melnyk/training/App.java
@@ -7,17 +7,18 @@ public class App {
 
     private Client client;
     private ConsoleEventLogger eventLogger;
+
     public static void main(String[] args) {
         App app = new App();
 
         app.client = new Client("1", "John");
-           app.eventLogger = new ConsoleEventLogger();
+        app.eventLogger = new ConsoleEventLogger();
         app.logEvent("Some event for user 1 ");
     }
 
     private void logEvent(String msg) {
         String message = msg.replaceAll(client.getId(), client.getFullName());
-         eventLogger.logEvent(message);
+        eventLogger.logEvent(message);
 
     }
 }
--- src/main/java/ua/melnyk/training/beans/Client.java
+++ src/main/java/ua/melnyk/training/beans/Client.java
@@ -6,6 +6,7 @@ public class Client {
     private String fullName;
 
     public Client(String id, String fullName) {
+        super();
         this.id = id;
         this.fullName = fullName;
     }
